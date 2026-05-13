b132b049b6e216c460d41b7a55559f6cef165305
FleterEwenn <fleter.ewenn@gmail.com>
2026-05-13 20:19:34 +0200

    Create repo

---
+++ main.py
@@ -0,0 +1,43 @@
+from overpass import get_path, calculer_dist
+import time
+
+list_path = False
+
+while not list_path:
+    list_path = get_path()
+    time.sleep(0.01)
+
+graphe = {}
+for path in list_path:
+    list_point = [(pt["lat"], pt["lon"]) for pt in path["geometry"]]
+
+    for i in range(1, len(list_point)):
+        point1 = (round(list_point[i-1][0], 5), round(list_point[i-1][1], 5))
+        point2 = (round(list_point[i][0], 5), round(list_point[i][1], 5))
+        dist = calculer_dist(point1, point2)
+        graphe[point1] = graphe.get(point1, []) + [(point2, dist)]
+        graphe[point2] = graphe.get(point2, []) + [(point1, dist)]
+
+start = list(graphe.keys())[0]
+dijkstra = {}
+for n in graphe:
+    dijkstra[n] = (float("inf"), None)
+
+dijkstra[start] = (0, start)
+
+file = [(0, start)]
+
+while len(file) > 0:
+    current_dist, current_node = file.pop(0)
+
+    if not current_dist > dijkstra[current_node][0]:
+
+        for neighbor, neighbor_dist in graphe[current_node]:
+            new_dist = current_dist + neighbor_dist
+            if new_dist < dijkstra[neighbor][0]:
+                dijkstra[neighbor] = (new_dist, current_node)
+
+                file.append((new_dist, neighbor))
+
+with open("dijkstra.txt", "w") as f:
+    f.write(str(dijkstra))
---
+++ overpass.py
@@ -0,0 +1,27 @@
+import requests
+import math
+
+url = "https://overpass.kumi.systems/api/interpreter"
+
+overpass_request = """
+[out:json][timeout:20];
+(
+	way["highway"](around:3000, 45.0320272, 1.8060011);
+	-
+	way["highway"~"primary|tertiary"](around:3000, 45.0320272, 1.8060011);
+);
+out geom;"""
+
+def get_path()->dict:
+    response = requests.get(url, data=overpass_request)
+    if response.status_code == 200:
+        data = response.json()
+        return data["elements"]
+    else:
+        return False
+def calculer_dist(point1, point2):
+    midlat = (point1[0] + point2[0])/2
+    dy = (point1[0] - point2[0]) * 110540
+    dx = (point1[1] - point2[1]) * 111320 * math.cos(math.radians(midlat))
+
+    return math.sqrt(dx**2 + dy**2)
